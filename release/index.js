d9c785e6a19a60281934f85bf6bf73baf190aac6
Albermonte <alberto.monterroso@hotmail.com>
2020-07-31 13:12:00 +0200

    Added files

---
+++ release/index.js
@@ -0,0 +1,28 @@
+const { exec } = require("child_process");
+const os = require("os");
+const fs = require("fs");
+const Windows = require.resolve("../release_windows.bat");
+const LinuxAndMac = require.resolve("../release_linux.sh");
+
+fs.chmodSync(LinuxAndMac, "755");
+
+const log = (error, stdout, stderr) => {
+  if (error) {
+    console.log(`error: ${error.message}`);
+    return;
+  }
+  if (stderr) {
+    console.log(`stderr: ${stderr}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+};
+
+const osType = os.type();
+
+console.log(`Optimizing build for ${osType}, this will take some time`);
+
+if (osType === "Linux") exec(LinuxAndMac, log);
+else if (osType === "Darwin") exec(LinuxAndMac, log);
+else if (osType === "Windows_NT") exec(Windows, log);
+else throw new Error("Unsupported OS found: " + osType);
